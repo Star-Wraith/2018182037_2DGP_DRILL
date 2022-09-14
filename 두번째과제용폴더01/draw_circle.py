19723cce49ac1631f26cce3b09142c2ed0121990
SangJun Cho <fpdltm456@gmail.com>
2022-09-14 17:38:39 +0900

    2022-09-14 모눈종이

---
+++ 두번째과제용폴더01/draw_circle.py
@@ -0,0 +1,167 @@
+import turtle
+
+
+turtle.reset()
+
+i=10
+while(i>5):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+
+turtle.forward(100)
+i=10 
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+i=10
+turtle.forward(100)
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i =i-1
+i=10
+turtle.forward(100)
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+i=10
+turtle.forward(100)
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+    
+turtle.goto(100,0)
+turtle.right(90)
+i=10
+while(i>5):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+
+turtle.forward(100)
+i=10 
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+i=10
+turtle.forward(100)
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i =i-1
+i=10
+turtle.forward(100)
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+i=10
+turtle.forward(100)
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+    
+turtle.goto(200,0)
+turtle.right(90)
+i=10
+while(i>5):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+
+turtle.forward(100)
+i=10 
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+i=10
+turtle.forward(100)
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i =i-1
+i=10
+turtle.forward(100)
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+i=10
+turtle.forward(100)
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+    
+turtle.goto(300,0)
+turtle.right(90)
+i=10
+while(i>5):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+
+turtle.forward(100)
+i=10 
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+i=10
+turtle.forward(100)
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i =i-1
+i=10
+turtle.forward(100)
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+i=10
+turtle.forward(100)
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+turtle.goto(400,0)
+turtle.right(90)
+i=10
+while(i>5):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+
+turtle.forward(100)
+i=10 
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+i=10
+turtle.forward(100)
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i =i-1
+i=10
+turtle.forward(100)
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1
+i=10
+turtle.forward(100)
+while(i>6):
+    turtle.forward(100)
+    turtle.left(90)
+    i=i-1    
+    
