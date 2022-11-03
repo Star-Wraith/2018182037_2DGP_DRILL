d7a394f888ca05c7f9e4247c4f4d9e172a3537c7
SangJun Cho <fpdltm456@gmail.com>
2022-11-03 14:15:13 +0900

    DRILL12

---
+++ DRILL/12/grass.py
@@ -0,0 +1,17 @@
+from pico2d import *
+
+class Grass:
+    def __init__(self, x=400, y=30):
+        self.x = x
+        self.y = y
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
+    def update(self):
+        pass
+
+
+
